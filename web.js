c254d563ff7ff54dae2ac6cfe673a6b10611d0fc
Tara Balakrishnan <taragb@gmail.com>
2013-07-10 23:09:01 +0000

    read file attempt3

--- web.js
+++ web.js
@@ -3,18 +3,18 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-fs.readFileSync('index.html', function (err, data) {
+/*fs.readFileSync('index.html', function (err, data) {
     if (err) {
 	return console.log(err);
     }
     app.get'/', function(request, response) {
 	response.send(data.toString());
     });
-});
+});*/
 
-//app.get('/', function(request, response) {
- // response.send(text);
-//});
+app.get('/', function(request, response) {
+   response.send('text');
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
